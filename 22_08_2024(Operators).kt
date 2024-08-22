ad858ca9aaf0a3d27ed9a77ed1dffc28ff604ed1
sambhav sharma <118333908+SambhavSharmaz@users.noreply.github.com>
2024-08-22 13:29:44 +0530

    Create 22_08_2024(Operators).kt

---
+++ 22_08_2024(Operators).kt
@@ -0,0 +1,18 @@
+// Arithmetic and Logical Operators
+fun main() {
+    val a=10
+    val b=10
+    val tru= true
+    val fal= false
+    val add=a + b
+    println("add : $add")
+    println("sub : ${a-b}")
+    println("multi : ${a*b}")
+    println("div : ${a/b}")
+    if (a==5){
+        print(tru)
+    }
+    else{
+        print(fal)
+    }
+}
