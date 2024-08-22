8d35c45568746a23f069c5ba500eba4056297477
sambhav sharma <118333908+SambhavSharmaz@users.noreply.github.com>
2024-08-22 13:53:25 +0530

    Update 22_08_2024(Operators).kt

--- 22_08_2024(Operators).kt
+++ 22_08_2024(Operators).kt
@@ -1,18 +1,46 @@
-// Arithmetic and Logical Operators
+// Arithmetic , Assignment, Logical, Operators
 fun main() {
-    val a=10
-    val b=10
-    val tru= true
-    val fal= false
-    val add=a + b
+    var a=10
+    var b=10
+    var c= null
+    var tru= true
+    var fal= false
+    var add=a + b
     println("add : $add")
     println("sub : ${a-b}")
     println("multi : ${a*b}")
     println("div : ${a/b}")
+    println("mod : ${a%b}")
     if (a==5){
-        print(tru)
+        println(tru)
     }
     else{
-        print(fal)
+        println(fal)
     }
+    println("greater than: ${a>b}")
+    println("less than: ${a<b}")
+    println("greater than equal to: ${a>=b}")
+    println("less than equal to: ${a<=b}")
+    println("equals to: ${a==b}")
+    println("not equals to: ${a!=b}")
+    println("null: ${c}")
+    a=a+b
+    println("$a")
+    a=a-b
+    println("$a")
+    b=b*a
+    println("$b")
+    a=a/b
+    println("$a")
+    a=a%b
+    println("$a")
+    +a
+    println("$a")
+    -a
+    println("$a")
+    ++a
+    println("$a")
+    --a
+    println("$a")
+    // !a
 }
